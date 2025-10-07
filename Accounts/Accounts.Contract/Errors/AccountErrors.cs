141c786923843fad3d453a46cf41a6e6278266a4
Đỗ Thị Thùy Vân <dovan032004@gmail.com>
2025-10-07 13:45:09 +0700

    [system] delete Accounts directory

--- Accounts/Accounts.Contract/Errors/AccountErrors.cs
+++
@@ -1,9 +0,0 @@
-namespace Accounts.Contract.Errors
-{
-    public class AccountErrors
-    {
-        public const string ACCOUNT_NOT_FOUND = "ACCOUNT_NOT_FOUND";
-        public const string ACCOUNT_ALREADY_EXISTS = "ACCOUNT_ALREADY_EXISTS";
-        public const string WRONG_PASSWORD = "WRONG_PASSWORD";
-    }
-}
--- Accounts/Accounts.Data/Models/AccountTokenModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Accounts.Data.Models
-{
-    public class AccountTokenModel
-    {
-        public required string AccessToken { get; set; }
-        public required string RefreshToken { get; set; }
-    }
-}
