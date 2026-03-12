e5b6cd04fc1c086ef167179f117b2301c6972cd4
ThuyVan032004 <dovan032004@gmail.com>
2026-03-12 11:51:01 +0700

    update deploy.sh

--- Zenit.Management/Zenit.Management.Host/deploy.sh
+++ Zenit.Management/Zenit.Management.Host/deploy.sh
@@ -6,8 +6,6 @@ usage() {
     echo "Available commands:"
     echo "build             Build Docker image for the app and monitoring service"
     echo "up                Run app and monitoring service container"
-    # echo "push              Push Docker image to registry"
-    # echo "build_and_push    Build and push Docker image to registry"
 }
 
 if [[ -z "$cmd" ]]; 
@@ -29,13 +27,7 @@ build() {
 
 up() {
     docker run -d --env-file Zenit.Management/Zenit.Management.Host/.env --network infrastructures_default -p 5212:5212 --name zenit-management-api $APP_IMAGE_NAME:latest
-    # docker run --network shared-network -p 8767:8767 --name monitoring-service $MONITORING_SERVICE_IMAGE_NAME
 }
-
-# push() {
-#     docker push $APP_IMAGE_NAME:$tag
-#     docker push $IMAGE_NAME:latest
-# }
 
 case $cmd in
     build)
@@ -44,10 +36,6 @@ case $cmd in
     up)
         up
         ;;
-    # build_and_push)
-    #     build
-    #     push
-    #     ;;
     *)
         echo "Invalid command: $cmd"
         usage
